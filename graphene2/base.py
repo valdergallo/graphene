56c1b5ee020fa8c004117271fc465968a2d94935
Valder Gallo <valdergallo@gmail.com>
2016-10-27 21:27:55 -0200

    pure python graphene

---
+++ graphene2/base.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+import six
+
+
+class GraphQLMetabase(object):
+
+    def __init__(self, *kwarg):
+        self.__TYPE__ = None
+
+    def __repr__(self):
+        return 'GraphQL Type({0}) {1}'.format(self.__TYPE__, self.__class__.__name__)
+
+    def to_grapheql(self):
+        return "{}"
+
+
+class GraphQL(type):
+    def __new__(meta, name, bases, dict):
+        cls = type.__new__(meta, name, (GraphQLMetabase,) + bases, dict)
+        return cls
+
+
+class Node(GraphQL):
+    __TYPE__ = 'node'
+
+
+class Query(GraphQL):
+    __TYPE__ = 'query'
+
+
+class Mutation(GraphQL):
+    __TYPE__ = 'mutation'
---
+++ graphene2/elements.py
@@ -0,0 +1,77 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+from collections import OrderedDict
+import json
+import traceback
+import sys
+import six
+import re
+
+
+class GrapheneObject(OrderedDict):
+    __slot__ = ('label', 'value', 'description', 'output')
+
+    def __init__(self, label=None, value=None):
+        self.value = value
+        if not label:
+            (filename, line_number, function_name, text)=traceback.extract_stack()[-2]
+            label = text[:text.find('=')].strip()
+        self.__dict__['label'] = self.to_camel(label)
+
+    def __setattr__(self, attr, value):
+        if attr not in self.__slot__:
+            raise AttributeError('Invalid attribute ({})'.format(attr))
+        if value is not None:
+            try:
+                self.__dict__[attr] = self.validate(value)
+            except:
+                print "Validation error for {}:".format(attr)
+                print '-' * 60
+                traceback.print_exc(file=sys.stdout)
+                print '-' * 60
+
+        self.__dict__[attr] = value
+
+    def to_camel(self, value):
+        components = str(value).split('_')
+        # We capitalize the first letter of each component except the first one
+        # with the 'title' method and join them together.
+        return components[0] + "".join(x.title() for x in components[1:])
+
+    def validate(self, value):
+        return value
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+
+class String(GrapheneObject):
+
+    def validate(self, value):
+        return str(value)
+
+
+class Int(GrapheneObject):
+
+    def validate(self, value):
+        return int(value)
+
+
+class Float(GrapheneObject):
+
+    def validate(self, value):
+        return float(value)
+
+
+class List(GrapheneObject):
+
+    def validate(self, value):
+        return list(value)
+
+
+class Field(GrapheneObject):
+
+    def validate(self, value):
+        if not isinstance(value, GrapheQLElement):
+            raise ValueError('Elements must be one valid GraphQL.Element')
+        return value
